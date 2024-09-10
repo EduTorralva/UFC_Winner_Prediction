5d7ceb3782729ad0fb881108eae383fc6e1ebc80
EduTorralva <141433495+EduTorralva@users.noreply.github.com>
2024-09-10 00:28:03 -0500

    Update predict.py

--- scr/predict.py
+++ scr/predict.py
@@ -1,1 +1,41 @@
+# Código de Scoring
+###################
 
+import pandas as pd
+import xgboost as xgb
+import pickle
+import matplotlib.pyplot as plt
+from sklearn.metrics import *
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import accuracy_score
+from sklearn.dummy import DummyClassifier
+import sys, warnings, os
+
+
+# Cargar la tabla transformada
+def score_model(filename, scores):
+    ufc_master_ds  = pd.read_csv(os.path.join('../data/processed', filename)).set_index('Winner')
+    print(filename, ' cargado correctamente')
+    # Leemos el modelo entrenado para usarlo
+    package = '../models/best_model.pkl'
+    model = pickle.load(open(package, 'rb'))
+    print('Modelo importado correctamente')
+    # Predecimos sobre el set de datos de Scoring    
+    res = model.predict(ufc_master_ds).reshape(-1,1)
+    pred = pd.DataFrame(res, columns=['PREDICT'])
+    pred.to_csv(os.path.join('../data/scores/', scores))
+    print(scores, 'exportado correctamente en la carpeta scores')
+
+
+# Scoring desde el inicio
+def main():
+    df = score_model('ufc-master.csv','final_score.csv')
+    print('Finalizó el Scoring del Modelo')
+
+
+if __name__ == "__main__":
+    main()
